feafd389946300191f44686da631f2afa405b1f4
ale-neto <alexandreneto.dev@gmail.com>
2020-08-23 09:24:26 -0300

    update

---
+++ agendaliveapi/src/main/java/com/spring/agendalive/repository/LiveRepository.java
@@ -0,0 +1,15 @@
+package com.spring.agendalive.repository;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.agendalive.document.LiveDocument;
+
+
+public interface LiveRepository extends JpaRepository<LiveDocument, String> {
+    Page<LiveDocument> findByLiveDateAfterOrderByLiveDateAsc(LocalDateTime date, Pageable pageable);
+    Page<LiveDocument> findByLiveDateBeforeOrderByLiveDateDesc(LocalDateTime date, Pageable pageable);
+}
