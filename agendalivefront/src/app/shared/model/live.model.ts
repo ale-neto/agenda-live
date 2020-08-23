f63a8af49f95f2a407d84a15ccb5bccc29afdda3
ale-neto <alexandreneto.dev@gmail.com>
2020-08-23 10:54:01 -0300

    update

---
+++ agendalivefront/src/app/shared/model/live.model.ts
@@ -0,0 +1,9 @@
+export class live{
+    id: string;
+    liveName: string;
+    channelName: string;
+    liveData: string;
+    liveTime: string;
+    liveLink: string;
+    resgistrationDate: string;
+}
